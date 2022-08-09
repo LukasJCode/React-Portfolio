8d4e54e0d6ea7318670635aae02b370c2b681f98
LukasJCode <89071785+LukasJCode@users.noreply.github.com>
2022-08-09 15:20:07 +0200

    Secrets project added

--- src/data/projectdata.js
+++ src/data/projectdata.js
@@ -1,7 +1,7 @@
 const projects = [
     {
         id: 1,
-        title: "Note Webste",
+        title: "Note Website",
         shortDescription: "A simple website used to add/remove notes for yourself",
         thumbnailImg: "/images/noteweb/Thumbnail.png",
         category: "web",
@@ -10,36 +10,51 @@ const projects = [
             "/images/noteweb/Screenshot_2.png",
             "/images/noteweb/Screenshot_3.png"
         ],
-        description: "A simple website used to add/remove notes for yourself. For the front-end React.js was used. For the back end Node. More specifically Express framework. Information is stored in MongoDB database. Some other libraries used in the project: mongoose, body-parser, cors, axios.",
+        description: "A simple website used to add/remove notes for yourself. For the front-end React.js was used. For the back end Node. More specifically Express framework. Information is stored in MongoDB database. Some other libraries used in the project: mongoose, cors, axios.",
     },
     {
         id: 2,
-        title: "Bubble Title",
-        shortDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
-        thumbnailImg: "/images/code.png",
+        title: "Simon Game",
+        shortDescription: "Simon the game is a childrens toy, this is website replica of it",
+        thumbnailImg: "/images/simonweb/Thumbnail.png",
         category: "web",
         imgs:[
-            "/images/code.png",
-            "/images/code.png",
-            "/images/code.png"
+            "/images/simonweb/Screenshot_3.png",
+            "/images/simonweb/Screenshot_1.png",
+            "/images/simonweb/Screenshot_2.png"
         ],
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
-    },
-    {
-        id: 5,
-        title: "Bubble Title",
-        shortDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
-        thumbnailImg: "/images/code.png",
-        category: "web",
-        imgs:[
-            "/images/code.png",
-            "/images/code.png",
-            "/images/code.png"
-        ],
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
+        description: "A website dedicated to Simons childrens game. A digital replica of it. Made with HTML CSS and JavaScript.",
     },
     {
         id: 3,
+        title: "Secrets",
+        shortDescription: "Secrets is a website where users can share their secrets anonymously",
+        thumbnailImg: "/images/secretsweb/Thumbnail.png",
+        category: "web",
+        imgs:[
+            "/images/secretsweb/Screenshot_1.png",
+            "/images/secretsweb/Screenshot_2.png",
+            "/images/secretsweb/Screenshot_3.png",
+            "/images/secretsweb/Screenshot_4.png",
+            "/images/secretsweb/Screenshot_5.png"
+        ],
+        description: "Secrets is a website where people can share their secrets securely and anonymously. Front end made with HTML CSS and powered by ejs library. Back end was made with node.js using: express.js, mongoose.js, passport.js packages. Google Authentification is also enabled.",
+    },
+    {
+        id: 4,
+        title: "Bubble Title",
+        shortDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
+        thumbnailImg: "/images/code.png",
+        category: "games",
+        imgs:[
+            "/images/code.png",
+            "/images/code.png",
+            "/images/code.png"
+        ],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
+    },
+    {
+        id: 4,
         title: "Bubble Title",
         shortDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
         thumbnailImg: "/images/code.png",
